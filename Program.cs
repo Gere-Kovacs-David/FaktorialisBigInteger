3b0e8321446d1474676b2426812260d6d2574f2e
Dávid Gere Kovács <geresmithdavid@gmail.com>
2020-09-30 11:27:21 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+//biginteger osztály elérése -> references -> add referenc sys numrics
+
+namespace FaktoriálisBigInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktoriális meghatározása 20-40 között");
+            for (int szám = 20; szám <= 40; szám++)
+            {
+                BigInteger faktor = 1;
+                for (int i = 2; i <= szám ; i++)
+                {
+                    faktor = faktor * i;
+                }
+                Console.WriteLine($"{szám} != {faktor}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
